a193a799d2518d142b7bb723ed06210a18646bdb
Carlos Saraiva <carlosedosaraiva@gmail.com>
2022-09-09 00:35:12 +0000

    chore: add fade in up animation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,105 @@
-import React, { useRef, useEffect, useImperativeHandle, forwardRef } from "react";
-import styled from "styled-components/native";
+import React, { useRef, useImperativeHandle, forwardRef, useState } from "react";
 import { Animated, View, Text, Button } from 'react-native';
 
-const shake = {
-  from: 0,
-  to: 10
+const animations = {
+  label: "shake",
+  shake: {
+    label: "shake",
+    style: (animationValue) => {
+      return {
+        transform: [
+          {
+            translateX: animationValue
+          }
+        ]
+      }
+    },
+    animation: (animationValue) => {
+      return Animated.loop(
+        Animated.sequence([
+          Animated.timing(
+            animationValue,
+            {
+              toValue: -10,
+              duration: 30,
+            }
+          ),
+          Animated.timing(
+            animationValue,
+            {
+              toValue: 0,
+              duration: 30,
+            }
+          ),
+          Animated.timing(
+            animationValue,
+            {
+              toValue: 10,
+              duration: 30,
+            }
+          ),
+          Animated.timing(
+            animationValue,
+            {
+              toValue: 0,
+              duration: 30,
+            }
+          ),
+        ]), {
+        iterations: 2
+      })
+    }
+  },
+  fadeInUp: {
+    label: "fade in up",
+    animation: (animationValue) => {
+      return Animated.loop(Animated.timing(animationValue, {
+        toValue: 1,
+        duration: 1000,
+      }
+      ), {
+        iterations: 1
+      })
+    },
+    style: (animationValue) => {
+      return {
+        opacity: animationValue,
+        transform: [
+          {
+            translateY: animationValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [100, 0]
+            })
+          }
+        ]
+      }
+    }
+  }
 }
 
 
 let ShakeView = (props, ref) => {
-  const fadeAnim = useRef(new Animated.Value(0)).current
-
-  const shake = () => {
-    Animated.loop(
-    Animated.sequence([
-      Animated.timing(
-        fadeAnim,
-        {
-          toValue: -10,
-          duration: 30,
-        }
-      ),
-      Animated.timing(
-        fadeAnim,
-        {
-          toValue: 0,
-          duration: 30,
-        }
-      ),
-      Animated.timing(
-        fadeAnim,
-        {
-          toValue: 10,
-          duration: 30,
-        }
-      ),
-      Animated.timing(
-        fadeAnim,
-        {
-          toValue: 0,
-          duration: 30,
-        }
-      ),
-    ]), {
-      iterations: 2
-    }).start()
-  }
+  const animationValue = useRef(new Animated.Value(0)).current
+  const [currentAnimation, setCurrentAnimation] = useState()
 
   useImperativeHandle(ref, () => ({
     shake: () => {
-      shake()
+      setCurrentAnimation("shake");
+      return new Promise((resolve) => {
+        animations.shake.animation(animationValue).start((result) => resolve({
+          ...result,
+          ...animations.shake
+        }));
+      })
+    },
+    fadeInUp: () => {
+      setCurrentAnimation("fadeInUp");
+      return new Promise((resolve) => {
+        animations.fadeInUp.animation(animationValue).start((result) => resolve({
+          ...result,
+          ...animations.fadeInUp
+        }));
+      })
     }
   }))
 
@@ -57,9 +107,8 @@ let ShakeView = (props, ref) => {
     <Animated.View
       style={{
         ...props.style,
-        transform: [{
-          translateX: fadeAnim
-        }]
+        ...animations[currentAnimation]?.style(animationValue),
+
       }}
     >
       {props.children}
@@ -70,16 +119,30 @@ let ShakeView = (props, ref) => {
 ShakeView = forwardRef(ShakeView)
 
 const App = () => {
+  const [currentAnimation, setCurrentAnimation] = useState()
+  const [animationStatus, setAnimationStatus] = useState("idle")
   const shakeViewRef = useRef()
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <ShakeView ref={shakeViewRef} style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
-        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Shake</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Animation status: {animationStatus}</Text>
+      <ShakeView ref={shakeViewRef} style={{ width: 250, height: 50, backgroundColor: 'powderblue' }}>
+        <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>{currentAnimation}</Text>
       </ShakeView>
-      <Button title="animate" onPress={() => {
-        shakeViewRef.current.shake()
-      }}/>
+      <View style={{ marginTop: 100 }}>
+        <Button title="shake" onPress={async () => {
+          setCurrentAnimation("shake")
+          setAnimationStatus("animating")
+          await shakeViewRef.current.shake()
+          setAnimationStatus("finished")
+        }} />
+        <Button title="fadeInUp" onPress={async () => {
+          setCurrentAnimation("fadeInUp")
+          setAnimationStatus("animating")
+          await shakeViewRef.current.fadeInUp()
+          setAnimationStatus("finished")
+        }} />
+      </View>
     </View>
   )
 }
@@ -87,10 +150,3 @@ const App = () => {
 
 
 export default App;
-
-const Header = styled.View`
-  background-color: black;
-`;
-const H1 = styled.Text`
-  color: white;
-`;
